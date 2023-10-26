a0efe01b5eeeba443c8f4b75a5c49bf8c4e3f8d2
jonashaus <60618352+jonashaus@users.noreply.github.com>
2023-10-27 00:30:12 +0200

    Improved breakpoint behavior of homepage

--- src/app/page.js
+++ src/app/page.js
@@ -12,14 +12,14 @@ export default function Home() {
   const session = useSession();
   return (
     <div
-      className="container flex flex-col justify-center items-center space-y-8 md:flex-row md:justify-between "
+      className="container flex flex-col justify-center items-center space-y-8 md:flex-row md:justify-between"
       style={{ minHeight: "91vh" }}
     >
-      <div className="text-center md:text-start">
+      <div className="shrink-0 text-center md:text-start">
         <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
           LifeIndex
         </h1>
-        <p className="scroll-m-20 text-xl text-gray-500 lg:text-2xl">
+        <p className="scroll-m-20 text-xl text-gray-500 lg:text-2xl nowrap">
           A place to track your habits
         </p>
         {session && (
@@ -51,13 +51,13 @@ export default function Home() {
           </p>
         )}
       </div>
-      <div className="flex justify-center md:justify-end">
+      <div className="flex justify-center md:justify-end max-w-xl">
         <Image
           src="/home.svg"
           alt="Woman with checklist"
           width={1}
           height={1}
-          style={{ width: "70%", height: "auto" }}
+          style={{ width: "100%", height: "auto" }}
         />
       </div>
     </div>
